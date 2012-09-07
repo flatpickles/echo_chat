31db696d06cef8800f2ac85a4b923343316d6187
Matt Nichols <matt@mattnichols.net>
2012-09-07 15:17:39 -0400

    fixing indentation

--- client.c
+++ client.c
@@ -19,88 +19,88 @@ void *recv_loop(void *socket);
 pthread_t in_thread, out_thread;
 
 void *send_loop(void *socket) {
-  int rv, socket_fd = (int)socket;
-  while (1) {
-    char input[MSG_BUFFER_SIZE];
-    if (fgets(input, sizeof(input), stdin) != NULL) {
-      input[strlen(input)-1] = 0; // remove newline
-      if (!strcmp(input, "quit")) {
-        exit(0);
-      }
-      if ((rv = send(socket_fd, input, strlen(input)*4, 0)) < 0) {
-        perror("socket failure during send");
-        exit(-1);
-      }
+    int rv, socket_fd = (int)socket;
+    while (1) {
+        char input[MSG_BUFFER_SIZE];
+        if (fgets(input, sizeof(input), stdin) != NULL) {
+            input[strlen(input)-1] = 0; // remove newline
+            if (!strcmp(input, "quit")) {
+                exit(0);
+            }
+            if ((rv = send(socket_fd, input, strlen(input)*4, 0)) < 0) {
+                perror("socket failure during send");
+                exit(-1);
+            }
+        }
+        memset(input, 0, sizeof(input));
+        fflush(stdout);
     }
-    memset(input, 0, sizeof(input));
-    fflush(stdout);
-  }
-  return NULL;
+    return NULL;
 }
 
 void *recv_loop(void *socket) {
-  int rv, socket_fd = (int)socket;
-  while (1) {
-    char sent[MSG_BUFFER_SIZE];
-    if ((rv = recv(socket_fd, sent, MSG_BUFFER_SIZE, 0)) == -1) {
-      perror("socket failure during recv");
-      exit(-1);
+    int rv, socket_fd = (int)socket;
+    while (1) {
+        char sent[MSG_BUFFER_SIZE];
+        if ((rv = recv(socket_fd, sent, MSG_BUFFER_SIZE, 0)) == -1) {
+            perror("socket failure during recv");
+            exit(-1);
+        }
+        if (rv == 0) {
+            printf("[server has closed the connection]\n");
+            exit(0);
+        }
+        printf("[server] %s\n", sent);
+        memset(sent, 0, sizeof(sent));
+        fflush(stdout);
     }
-    if (rv == 0) {
-      printf("[server has closed the connection]\n");
-      exit(0);
-    }
-    printf("[server] %s\n", sent);
-    memset(sent, 0, sizeof(sent));
-    fflush(stdout);
-  }
-  return NULL;
+    return NULL;
 }
 
 int main(int argc, char **argv) {
-  // check arguments
-  if (argc != 3) {
-    printf("usage: client server_hostname port\n");
+    // check arguments
+    if (argc != 3) {
+        printf("usage: client server_hostname port\n");
+        return 0;
+    }
+    char *ip_str = argv[1];
+    char *port_str = argv[2];
+    printf("Connecting with %s on port %s...\n", ip_str, port_str);
+
+    // establish the connection
+
+    int rv, socket_fd;
+    struct addrinfo hints, *servinfo;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET; // IPv4
+    hints.ai_socktype = SOCK_STREAM; // TCP
+
+    if ((rv = getaddrinfo(ip_str, port_str, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "\n getaddrinfo error: %s", gai_strerror(rv));
+        return -1;
+    }
+
+    if ((socket_fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1) {
+        perror("failed to create socket");
+        return -1;
+    }
+
+    int ret;
+    if ((ret = connect(socket_fd, servinfo->ai_addr, servinfo->ai_addrlen)) == -1) {
+        perror("failed to connect with server");
+        close(socket_fd);
+        return -1;
+    }
+
+    freeaddrinfo(servinfo);
+    printf("Connection established!\n");
+
+    // start the threads
+
+    pthread_create(&in_thread, NULL, send_loop, (void*)socket_fd);
+    pthread_create(&out_thread, NULL, recv_loop, (void*)socket_fd);
+    pthread_join(in_thread, NULL);
+    pthread_join(out_thread, NULL);
+
     return 0;
-  }
-  char *ip_str = argv[1];
-  char *port_str = argv[2];
-  printf("Connecting with %s on port %s...\n", ip_str, port_str);
-
-  // establish the connection
-
-  int rv, socket_fd;
-  struct addrinfo hints, *servinfo;
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family = AF_INET; // IPv4
-  hints.ai_socktype = SOCK_STREAM; // TCP
-
-  if ((rv = getaddrinfo(ip_str, port_str, &hints, &servinfo)) != 0) {
-    fprintf(stderr, "\n getaddrinfo error: %s", gai_strerror(rv));
-    return -1;
-  }
-
-  if ((socket_fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1) {
-    perror("failed to create socket");
-    return -1;
-  }
-
-int ret;
-  if ((ret = connect(socket_fd, servinfo->ai_addr, servinfo->ai_addrlen)) == -1) {
-    perror("failed to connect with server");
-    close(socket_fd);
-    return -1;
-  }
-
-  freeaddrinfo(servinfo);
-  printf("Connection established!\n");
-
-  // start the threads
-
-  pthread_create(&in_thread, NULL, send_loop, (void*)socket_fd);
-  pthread_create(&out_thread, NULL, recv_loop, (void*)socket_fd);
-  pthread_join(in_thread, NULL);
-  pthread_join(out_thread, NULL);
-
-  return 0;
 }
--- server_select.c
+++ server_select.c
@@ -17,103 +17,103 @@
 fd_set fd_list;
 
 int main(int argc, char **argv) {
-  // check arguments
-  if (argc != 2) {
-    printf("usage: server listening_port\n");
-    return 0;
-  }
-  char *port_str = argv[1];
+    // check arguments
+    if (argc != 2) {
+        printf("usage: server listening_port\n");
+        return 0;
+    }
+    char *port_str = argv[1];
 
-  // set up listening socket
+    // set up listening socket
 
-  int rv, socket_fd;
-  struct addrinfo hints, *servinfo;
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family = AF_INET; // IPv4
-  hints.ai_socktype = SOCK_STREAM; // TCP
+    int rv, socket_fd;
+    struct addrinfo hints, *servinfo;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET; // IPv4
+    hints.ai_socktype = SOCK_STREAM; // TCP
 
-  if ((rv = getaddrinfo(NULL, port_str, &hints, &servinfo)) != 0) {
-    fprintf(stderr, "\n getaddrinfo error: %s", gai_strerror(rv));
-    return -1;
-  }
-
-  if ((socket_fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) < 0) {
-    perror("\n Failed to create socket");
-    return -1;
-  }
-
-  if (bind(socket_fd, servinfo->ai_addr, servinfo->ai_addrlen) < 0) {
-    close(socket_fd);
-    fprintf(stderr, "server error: bind failure.\n");
-    return -1;
-  }
-
-  freeaddrinfo(servinfo);
-
-  if (listen(socket_fd, 10) < 0) {
-    perror("listen failure");
-    return -1;
-  }
-
-  printf("Listening for connections...\n");
-
-  // set up select + vars for accepting connections
-
-  fd_set fd_list_temp;
-  int fd_max, new_socket;
-  struct sockaddr_storage remote_addr;
-  socklen_t addr_len;
-  FD_ZERO(&fd_list);
-  FD_ZERO(&fd_list_temp);
-  fd_max = socket_fd;
-  FD_SET(socket_fd, &fd_list);
-
-  // run select loop
-
-  while (1) {
-    fd_list_temp = fd_list;
-
-    if (select(fd_max + 1, &fd_list_temp, NULL, NULL, NULL) == -1) {
-      perror("error on select call");
-      return -1;
+    if ((rv = getaddrinfo(NULL, port_str, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "\n getaddrinfo error: %s", gai_strerror(rv));
+        return -1;
     }
 
-    int i;
-    for (i = 0; i <= fd_max; i++) {
-      if (FD_ISSET(i, &fd_list_temp)) {
-        if (i == socket_fd) {
-          // new connection
-          addr_len = sizeof(remote_addr);
-          new_socket = accept(socket_fd, (struct sockaddr*)&remote_addr, &addr_len);
-          if (new_socket < 0) {
-            perror("error on accept call");
-          } else {
-            FD_SET(new_socket, &fd_list);
-            fd_max = fd_max < new_socket ? new_socket : fd_max;
-            printf("[%d] new connection\n", new_socket);
-          }
-        } else {
-          // recv from client
-          int rv;
-          char sent[MSG_BUFFER_SIZE];
-          if ((rv = recv(i, sent, MSG_BUFFER_SIZE, 0)) <= 0) {
-            if (rv != 0) perror("error on recv call");
-            close(i);
-            FD_CLR(i, &fd_list);
-            printf("[%d] connection closed\n", i);
-            continue;
-          }
+    if ((socket_fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) < 0) {
+        perror("\n Failed to create socket");
+        return -1;
+    }
 
-          // print and echo
-          printf("[%d] \"%s\"\n", i, sent);
-          char reply[MSG_BUFFER_SIZE];
-          sprintf(reply, "you sent \"%s\"", sent);
-          if ((rv = send(i, reply, strlen(reply)*4, 0)) < 0) {
-            perror("error on send call");
-          }
+    if (bind(socket_fd, servinfo->ai_addr, servinfo->ai_addrlen) < 0) {
+        close(socket_fd);
+        fprintf(stderr, "server error: bind failure.\n");
+        return -1;
+    }
+
+    freeaddrinfo(servinfo);
+
+    if (listen(socket_fd, 10) < 0) {
+        perror("listen failure");
+        return -1;
+    }
+
+    printf("Listening for connections...\n");
+
+    // set up select + vars for accepting connections
+
+    fd_set fd_list_temp;
+    int fd_max, new_socket;
+    struct sockaddr_storage remote_addr;
+    socklen_t addr_len;
+    FD_ZERO(&fd_list);
+    FD_ZERO(&fd_list_temp);
+    fd_max = socket_fd;
+    FD_SET(socket_fd, &fd_list);
+
+    // run select loop
+
+    while (1) {
+        fd_list_temp = fd_list;
+
+        if (select(fd_max + 1, &fd_list_temp, NULL, NULL, NULL) == -1) {
+            perror("error on select call");
+            return -1;
         }
-      }
-      fflush(stdout);
+
+        int i;
+        for (i = 0; i <= fd_max; i++) {
+            if (FD_ISSET(i, &fd_list_temp)) {
+                if (i == socket_fd) {
+                    // new connection
+                    addr_len = sizeof(remote_addr);
+                    new_socket = accept(socket_fd, (struct sockaddr*)&remote_addr, &addr_len);
+                    if (new_socket < 0) {
+                        perror("error on accept call");
+                    } else {
+                        FD_SET(new_socket, &fd_list);
+                        fd_max = fd_max < new_socket ? new_socket : fd_max;
+                        printf("[%d] new connection\n", new_socket);
+                    }
+                } else {
+                    // recv from client
+                    int rv;
+                    char sent[MSG_BUFFER_SIZE];
+                    if ((rv = recv(i, sent, MSG_BUFFER_SIZE, 0)) <= 0) {
+                        if (rv != 0) perror("error on recv call");
+                        close(i);
+                        FD_CLR(i, &fd_list);
+                        printf("[%d] connection closed\n", i);
+                        continue;
+                    }
+
+                    // print and echo
+                    printf("[%d] \"%s\"\n", i, sent);
+                    char reply[MSG_BUFFER_SIZE];
+                    sprintf(reply, "you sent \"%s\"", sent);
+                    if ((rv = send(i, reply, strlen(reply)*4, 0)) < 0) {
+                        perror("error on send call");
+                    }
+                }
+            }
+            fflush(stdout);
+        }
     }
-  }
 }
